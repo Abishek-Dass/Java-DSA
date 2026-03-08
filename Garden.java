93d0409eb59847d89470e6086f743311007056cc
Abishek-Dass <ad7954643@gmail.com>
2026-03-09 00:31:26 +0530

    Garden.java

---
+++ Garden.java
@@ -0,0 +1,12 @@
+public class Garden{
+    int a = 20;
+    int b =5;
+
+    void Multiply(){
+        System.out.println(a*b);
+    } 
+    public static void main(String[] args){
+        Garden garden = new Garden();
+        garden.Multiply();
+    }
+}
