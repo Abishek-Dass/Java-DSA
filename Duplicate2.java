bb1ad59a1b60d6360850dfd7593bef364039835e
Abishek D <ad7954643@gmail.com>
2026-08-17 23:41:03 +0530

    Duplicate2.java

--- Duplicate2.java
+++ Duplicate2.java
@@ -1,1 +1,15 @@
+import java.util.*;
+class Duplicate2{
+  public static void main(String args[]){
+    String str = "aabbcc";
+    boolean[] visited = new boolean[256];
+    for(int i=0; i<str.length(); i++){
+      char ch = str.charAt(i);
 
+      if(!visited[ch]){
+        System.out.print(ch);
+        visited[ch] = true;
+      }
+    }
+  }
+}
